21bb943d3e81dfc16327994c1cea478d986285ec
North1407 <vanbac14072001@gmail.com>
2023-06-18 17:56:50 +0700

    init project

---
+++ Web/FE/src/test/java/com/clothes/FeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clothes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
